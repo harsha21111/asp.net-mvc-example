b23c2266b16139f1e2af7aa8d0196c9632ff2ef8
harsha21111 <harshavardan2k17@gmail.com>
2021-04-14 11:31:37 +0530

    first commit

---
+++ MvcMovies/Data/MvcMoviesContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcMovies.Models;
+
+namespace MvcMovies.Data
+{
+    public class MvcMoviesContext : DbContext
+    {
+        public MvcMoviesContext(DbContextOptions<MvcMoviesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
